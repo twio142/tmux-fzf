e69536733dad826a32d15bafa4a728800a849086
twio142 <64556708+twio142@users.noreply.github.com>
2026-03-24 23:40:04 +0100

    feat(command): add yank and preview features
    - Add `ctrl-y` binding to yank selected command to tmux buffer.
    - Implement dynamic preview showing command documentation from man page.

--- scripts/command.sh
+++ scripts/command.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-MANPAGER="sh -c 'col -bx | bat -l man -p --color=always | less -R +/"
-OPTS="--nth=1,2 --accept-nth=1 --header='${BOLD}^H${OFF} help' \
-  --bind='ctrl-h:print(help)+accept'"
+OPTS="--nth=1,2 --accept-nth=1 --header='${BOLD}^H${OFF} help / ${BOLD}^Y${OFF} yank' \
+  --bind='ctrl-h:print(help)+accept' \
+  --bind='ctrl-y:execute-silent(printf {1} | tmux load-buffer -)' \
+  --preview-window=wrap \
+  --preview=\"l=\\\$(MANWIDTH=\\\$FZF_PREVIEW_COLUMNS man tmux | col -bx | grep -En '^     {1}($| \\[)' | cut -d: -f1); [ -z \\\${l} ] || MANWIDTH=\\\$FZF_PREVIEW_COLUMNS man tmux | col -bx | bat -l man -r \\\${l}: -H \\\${l}\""
 TMUX_FZF_OPTIONS="$TMUX_FZF_OPTIONS"
 CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$CURRENT_DIR/.envs"
@@ -14,8 +16,8 @@ if [[ -z "$target" ]]; then
   exit 0
 elif [[ "$target" == help* ]]; then
   target=$(echo "$target" | tail -n1)
-  line=$(tmux list-commands -F '#{command_list_name} #{s/(\[|\])/\\\1/g:command_list_usage}' | grep -F "$target" | head -n1)
-  MANPAGER="sh -c \"col -bx | bat -l man --paging always | less -R '+/${line}'\""
+  line=$(tmux list-commands -F '#{command_list_name} #{s/(\[|\])/\\\1/g:#{=10:command_list_usage}}' | awk -v t=$target '$1 == t {print $0}')
+  MANPAGER="sh -c \"col -bx | bat -l man --paging always | less -R '+/${line/% }.*'\""
   tmux splitw -v -e MANPAGER="$MANPAGER" man tmux
 else
   tmux command-prompt -I "$target"
